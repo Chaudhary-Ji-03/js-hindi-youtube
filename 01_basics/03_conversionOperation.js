8ab4eae3bf284d6b227651c44ebafa25f84a6eb1
Ritik Kumar Choudhary <142933502+Chaudhary-Ji-03@users.noreply.github.com>
2024-04-17 18:11:16 +0000

    js basics

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,54 @@
+// let score=null
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+//conversion
+
+//"33=>33
+//"33abc"=>NaN
+//true=>1;  false=>0
+
+// let isloggedIn=""
+// let boolleanIsloggedIn=Boolean(isloggedIn)
+// console.log(boolleanIsloggedIn);
+
+//1=>true;     0=>false
+//" "=>false
+//"Ritik"=>true
+
+// let somenumber=33
+
+// let stringnumber=string(somenumber)
+// console.log(typeof stringnumber)
+
+
+//******************************Operations *************************
+
+
+// let value=3
+// let negvalue=-value
+// console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)
+// console.log(2%2)
+
+
+// console.log("1"+2)
+// console.log("2"+1)
+// console.log("1"+2+3)
+// console.log(1+2+"3")
+// console.log(1+"2"+3)
+
+console.log(+true);
+
+let gamecounter=100
+gamecounter++
+console.log(gamecounter)
